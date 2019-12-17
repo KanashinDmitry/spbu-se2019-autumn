017c661040ad0e808e4dc8d823e970a9231c7bca
Kanashin_Dmitry <dmitrii.a.kanashin@gmail.com>
2019-12-17 16:12:46 +0300

    Time measurement fixe

--- Task06/time_measurement.cu
+++ Task06/time_measurement.cu
@@ -5,6 +5,8 @@
 #include "bitonic_sorts.cuh"
 #include "utils.cuh"
 
+#define AMOUNT_MEASUREMENTS = 20
+
 int main(char* args)
 {
     clock_t start;
@@ -30,16 +32,16 @@ int main(char* args)
             
             start = clock();
             bitonic_sort(temp_array, size);
-            timeCPU += (((double) (clock() - start)) / CLOCKS_PER_SEC) / 20;
+            timeCPU += ((double) (clock() - start)) / CLOCKS_PER_SEC;
 
             memcpy(temp_array, array, size_mem_array);
 
             start = clock();
             bitonic_sort_gpu(temp_array, size);
-            timeGPU += (((double) (clock() - start)) / CLOCKS_PER_SEC) / 20;
+            timeGPU += ((double) (clock() - start)) / CLOCKS_PER_SEC;
         }
 
-        printf("%ld %f %f\n", size, timeGPU, timeCPU);
+        printf("%ld %f %f\n", size, timeGPU / AMOUNT_MEASUREMENTS, timeCPU / AMOUNT_MEASUREMENTS);
         
         free(array);
         free(temp_array);
